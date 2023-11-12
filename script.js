c88324dcb224dbd75a40f44057f15bca83d898bf
Amin Gharibi <60180731+Amin-Gharibi@users.noreply.github.com>
2023-11-13 00:50:04 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let numbersCount;
+let isNumbersCountPrime;
+let secondPower;
+let fiboNums = [0, 1]
+let fiboCurrentIndex;
+let isInFibo;
+
+// getting the first 100 numbers of Fibonacci
+for (let l = 1; l < 100; l++) {
+    fiboCurrentIndex = fiboNums.length
+    if (l === 1) {
+        fiboNums.push(l)
+    } else {
+        fiboNums.push(fiboNums[fiboCurrentIndex - 1] + fiboNums[fiboCurrentIndex - 2])
+    }
+}
+
+// check numbers less than 100 if they are special number or not
+for (let i = 0; i < 100; i++) {
+    numbersCount = 0
+    for (let j = i; j !== 0;) {
+        j = Math.floor(j / 10)
+        numbersCount++
+    }
+    if (i === 0) {
+        numbersCount = 1
+    }
+
+    secondPower = numbersCount ** 2
+    isNumbersCountPrime = true
+    for (let k = 2; k < secondPower; k++) {
+        if (isNumbersCountPrime && numbersCount !== k) {
+            isNumbersCountPrime = numbersCount % k !== 0;
+        }
+    }
+    if (numbersCount === 1) {
+        isNumbersCountPrime = false
+    }
+
+    isInFibo = fiboNums.find(number => number === i)
+
+    if (isNumbersCountPrime && isInFibo) {
+        console.log(i)
+    }
+}
+
+alert('check console :)')
